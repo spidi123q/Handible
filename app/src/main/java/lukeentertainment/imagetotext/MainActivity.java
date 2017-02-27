33b2b5a66fa890cc5014aced074b3b9fd63df367
Unknown <matrixlukan@gmail.com>
2017-02-27 21:10:38 +0530

    project initialized

---
+++ app/src/main/java/lukeentertainment/imagetotext/MainActivity.java
@@ -0,0 +1,17 @@
+package lukeentertainment.imagetotext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
